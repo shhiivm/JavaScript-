bb49ae70ff08d683e9829568f623ec71a4e844f8
Shivam Singh <shhiivm@outlook.com>
2023-02-10 12:17:04 +0530

    initial commit

---
+++ JsVar.js
@@ -0,0 +1,29 @@
+// var x =10;
+// var y =20;
+// var z=30;
+// var sum = x+y+z;
+// console.log(sum); 
+
+
+// Local Variable
+// function abc(){ //Local Var
+//     var x = 10;
+//     console.log(x);
+// }
+// abc();
+
+
+// Global Variable
+// var data = 200;//Global var
+// function a(){
+//     console.log(data);
+// }
+// function b(){
+//     console.log(data);
+// }
+// a();
+// b();
+
+
+
+
